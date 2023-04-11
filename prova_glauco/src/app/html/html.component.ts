ed32fa220e5594b283a771d9070e692ec4523781
andre <duellinks090@gmail.com>
2023-04-11 09:58:04 -0300

    prova_1.4

--- prova_glauco/src/app/html/html.component.ts
+++ prova_glauco/src/app/html/html.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-html',
-  templateUrl: './html.component.html',
+  templateUrl: './html.component.css',
   styleUrls: ['./html.component.css']
 })
 export class HtmlComponent {
 
 }
+
