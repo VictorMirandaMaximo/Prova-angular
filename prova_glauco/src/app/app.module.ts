af4107b4e1ef15e0c1c2d7489c1adf70d0775ab1
andre <duellinks090@gmail.com>
2023-04-11 09:09:18 -0300

    prova_1.2

--- prova_glauco/src/app/app.module.ts
+++ prova_glauco/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HtmlComponent } from './html/html.component';
+import { JavaComponent } from './html/java/java.component';
+import { JavaJsComponent } from './html/java.js/java.js.component';
+import { HtmlComponentJsComponent } from './html/html.component.js/html.component.js.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HtmlComponent
+    HtmlComponent,
+    JavaComponent,
+    JavaJsComponent,
+    HtmlComponentJsComponent
   ],
   imports: [
     BrowserModule,
